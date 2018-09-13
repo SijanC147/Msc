fab46dc6214b5d8f4d51a887868ccdebdca56700
Sean Bugeja <seanbugeja23@gmail.com>
2018-09-13 13:28:12 +0200

    fixes edge case where redist to one class

--- tsaplay/utils.py
+++ tsaplay/utils.py
@@ -32,6 +32,7 @@ def token_filter(token):
 
 
 def re_dist(features, labels, distribution):
+    print(distribution)
     if type(distribution) == list:
         target_dists = distribution
     elif type(distribution) == dict:
@@ -42,151 +43,158 @@ def re_dist(features, labels, distribution):
         ]
 
     counts = [
-        len([l for l in labels if l == 1]),
-        len([l for l in labels if l == 0]),
         len([l for l in labels if l == -1]),
+        len([l for l in labels if l == 0]),
+        len([l for l in labels if l == 1]),
     ]
     target_counts = [0, 0, 0]
 
-    smallest_count_indices = [
-        i for i, x in enumerate(counts) if x == min(counts)
-    ]
-    if len(smallest_count_indices) != 1:
-        smallest_count_index = target_dists.index(
-            max([target_dists[i] for i in smallest_count_indices])
-        )
-    else:
-        smallest_count_index = smallest_count_indices[0]
+    try:
+        total_index = target_dists.index(1)
+        target_counts[total_index] = counts[total_index]
+        new_total = counts[total_index]
+    except ValueError:
+        smallest_count_indices = [
+            i for i, x in enumerate(counts) if x == min(counts)
+        ]
+        if len(smallest_count_indices) != 1:
+            smallest_count_index = target_dists.index(
+                max([target_dists[i] for i in smallest_count_indices])
+            )
+        else:
+            smallest_count_index = smallest_count_indices[0]
 
-    target_counts[smallest_count_index] = counts[smallest_count_index]
-    new_total = floor(
-        counts[smallest_count_index] / target_dists[smallest_count_index]
-    )
-    counts[smallest_count_index] = float("inf")
-    target_dists[smallest_count_index] = float("inf")
-
-    smallest_count_indices = [
-        i for i, x in enumerate(counts) if x == min(counts)
-    ]
-    if len(smallest_count_indices) != 1:
-        smallest_count_index = target_dists.index(
-            max([target_dists[i] for i in smallest_count_indices])
-        )
-    else:
-        smallest_count_index = smallest_count_indices[0]
-    smallest_count_index = counts.index(min(counts))
-    target_count = int(new_total * target_dists[smallest_count_index])
-    if target_count > counts[smallest_count_index]:
-        old_total = new_total
+        target_counts[smallest_count_index] = counts[smallest_count_index]
         new_total = floor(
             counts[smallest_count_index] / target_dists[smallest_count_index]
         )
-        target_counts = [
-            floor(t * (new_total / old_total)) for t in target_counts
-        ]
-        target_count = counts[smallest_count_index]
-    target_counts[smallest_count_index] = target_count
-    counts[smallest_count_index] = float("inf")
-    target_dists[smallest_count_index] = float("inf")
+        counts[smallest_count_index] = float("inf")
+        target_dists[smallest_count_index] = float("inf")
 
-    if new_total - sum(target_counts) > min(counts):
-        old_total = new_total
-        new_total = floor(min(counts) / min(target_dists))
-        target_counts = [
-            floor(t * (new_total / old_total)) for t in target_counts
+        smallest_count_indices = [
+            i for i, x in enumerate(counts) if x == min(counts)
         ]
-    target_counts[target_counts.index(0)] = new_total - sum(target_counts)
+        if len(smallest_count_indices) != 1:
+            smallest_count_index = target_dists.index(
+                max([target_dists[i] for i in smallest_count_indices])
+            )
+        else:
+            smallest_count_index = smallest_count_indices[0]
+        smallest_count_index = counts.index(min(counts))
+        target_count = int(new_total * target_dists[smallest_count_index])
+        if target_count > counts[smallest_count_index]:
+            old_total = new_total
+            new_total = floor(
+                counts[smallest_count_index]
+                / target_dists[smallest_count_index]
+            )
+            target_counts = [
+                floor(t * (new_total / old_total)) for t in target_counts
+            ]
+            target_count = counts[smallest_count_index]
+        target_counts[smallest_count_index] = target_count
+        counts[smallest_count_index] = float("inf")
+        target_dists[smallest_count_index] = float("inf")
 
-    postive_sample_indices = [i for i, x in enumerate(labels) if x == 1]
-    neutral_sample_indices = [i for i, x in enumerate(labels) if x == 0]
-    negative_sample_indices = [i for i, x in enumerate(labels) if x == -1]
-    new_features = {
-        "sentence": [""] * new_total,
-        "sentence_length": [0] * new_total,
-        "target": [""] * new_total,
-        "mappings": {
-            "left": [[]] * new_total,
-            "target": [[]] * new_total,
-            "right": [[]] * new_total,
-        },
-    }
-    new_labels = [None] * new_total
-
-    for _ in range(target_counts[0]):
-        random_index = choice(postive_sample_indices)
-        random_position = randrange(0, new_total)
-        while new_labels[random_position] is not None:
+        if new_total - sum(target_counts) > min(counts):
+            old_total = new_total
+            new_total = floor(min(counts) / min(target_dists))
+            target_counts = [
+                floor(t * (new_total / old_total)) for t in target_counts
+            ]
+        target_counts[target_counts.index(0)] = new_total - sum(target_counts)
+    finally:
+        negative_sample_indices = [i for i, x in enumerate(labels) if x == -1]
+        neutral_sample_indices = [i for i, x in enumerate(labels) if x == 0]
+        positive_sample_indices = [i for i, x in enumerate(labels) if x == 1]
+        new_features = {
+            "sentence": [""] * new_total,
+            "sentence_length": [0] * new_total,
+            "target": [""] * new_total,
+            "mappings": {
+                "left": [[]] * new_total,
+                "target": [[]] * new_total,
+                "right": [[]] * new_total,
+            },
+        }
+        new_labels = [None] * new_total
+        print(target_counts)
+        print(len(negative_sample_indices))
+        for _ in range(target_counts[0]):
+            random_index = choice(negative_sample_indices)
             random_position = randrange(0, new_total)
-        new_features["sentence"][random_position] = features["sentence"][
-            random_index
-        ]
-        new_features["sentence_length"][random_position] = features[
-            "sentence_length"
-        ][random_index]
-        new_features["target"][random_position] = features["target"][
-            random_index
-        ]
-        new_features["mappings"]["left"][random_position] = features[
-            "mappings"
-        ]["left"][random_index]
-        new_features["mappings"]["target"][random_position] = features[
-            "mappings"
-        ]["target"][random_index]
-        new_features["mappings"]["right"][random_position] = features[
-            "mappings"
-        ]["right"][random_index]
-        new_labels[random_position] = labels[random_index]
-        postive_sample_indices.remove(random_index)
-    for _ in range(target_counts[1]):
-        random_index = choice(neutral_sample_indices)
-        random_position = randrange(0, new_total)
-        while new_labels[random_position] is not None:
+            while new_labels[random_position] is not None:
+                random_position = randrange(0, new_total)
+            new_features["sentence"][random_position] = features["sentence"][
+                random_index
+            ]
+            new_features["sentence_length"][random_position] = features[
+                "sentence_length"
+            ][random_index]
+            new_features["target"][random_position] = features["target"][
+                random_index
+            ]
+            new_features["mappings"]["left"][random_position] = features[
+                "mappings"
+            ]["left"][random_index]
+            new_features["mappings"]["target"][random_position] = features[
+                "mappings"
+            ]["target"][random_index]
+            new_features["mappings"]["right"][random_position] = features[
+                "mappings"
+            ]["right"][random_index]
+            new_labels[random_position] = labels[random_index]
+            negative_sample_indices.remove(random_index)
+        for _ in range(target_counts[1]):
+            random_index = choice(neutral_sample_indices)
             random_position = randrange(0, new_total)
-        new_features["sentence"][random_position] = features["sentence"][
-            random_index
-        ]
-        new_features["sentence_length"][random_position] = features[
-            "sentence_length"
-        ][random_index]
-        new_features["target"][random_position] = features["target"][
-            random_index
-        ]
-        new_features["mappings"]["left"][random_position] = features[
-            "mappings"
-        ]["left"][random_index]
-        new_features["mappings"]["target"][random_position] = features[
-            "mappings"
-        ]["target"][random_index]
-        new_features["mappings"]["right"][random_position] = features[
-            "mappings"
-        ]["right"][random_index]
-        new_labels[random_position] = labels[random_index]
-        neutral_sample_indices.remove(random_index)
-    for _ in range(target_counts[2]):
-        random_index = choice(negative_sample_indices)
-        random_position = randrange(0, new_total)
-        while new_labels[random_position] is not None:
+            while new_labels[random_position] is not None:
+                random_position = randrange(0, new_total)
+            new_features["sentence"][random_position] = features["sentence"][
+                random_index
+            ]
+            new_features["sentence_length"][random_position] = features[
+                "sentence_length"
+            ][random_index]
+            new_features["target"][random_position] = features["target"][
+                random_index
+            ]
+            new_features["mappings"]["left"][random_position] = features[
+                "mappings"
+            ]["left"][random_index]
+            new_features["mappings"]["target"][random_position] = features[
+                "mappings"
+            ]["target"][random_index]
+            new_features["mappings"]["right"][random_position] = features[
+                "mappings"
+            ]["right"][random_index]
+            new_labels[random_position] = labels[random_index]
+            neutral_sample_indices.remove(random_index)
+        for _ in range(target_counts[2]):
+            random_index = choice(positive_sample_indices)
             random_position = randrange(0, new_total)
-        new_features["sentence"][random_position] = features["sentence"][
-            random_index
-        ]
-        new_features["sentence_length"][random_position] = features[
-            "sentence_length"
-        ][random_index]
-        new_features["target"][random_position] = features["target"][
-            random_index
-        ]
-        new_features["mappings"]["left"][random_position] = features[
-            "mappings"
-        ]["left"][random_index]
-        new_features["mappings"]["target"][random_position] = features[
-            "mappings"
-        ]["target"][random_index]
-        new_features["mappings"]["right"][random_position] = features[
-            "mappings"
-        ]["right"][random_index]
-        new_labels[random_position] = labels[random_index]
-        negative_sample_indices.remove(random_index)
+            while new_labels[random_position] is not None:
+                random_position = randrange(0, new_total)
+            new_features["sentence"][random_position] = features["sentence"][
+                random_index
+            ]
+            new_features["sentence_length"][random_position] = features[
+                "sentence_length"
+            ][random_index]
+            new_features["target"][random_position] = features["target"][
+                random_index
+            ]
+            new_features["mappings"]["left"][random_position] = features[
+                "mappings"
+            ]["left"][random_index]
+            new_features["mappings"]["target"][random_position] = features[
+                "mappings"
+            ]["target"][random_index]
+            new_features["mappings"]["right"][random_position] = features[
+                "mappings"
+            ]["right"][random_index]
+            new_labels[random_position] = labels[random_index]
+            positive_sample_indices.remove(random_index)
 
     return new_features, new_labels
 
